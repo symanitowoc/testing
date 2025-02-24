0c5d0961e69497099c83aba1cf88ea1ad114c3f3
symanitowoc <steven.yang@manitowoc.com>
2025-02-24 11:11:21 -0500

    trial

---
+++ suite_git_trial/shared/scripts/names.py
@@ -0,0 +1,6 @@
+# encoding: UTF-8
+
+from objectmaphelper import *
+
+gUI_Window_QQuickApplicationWindow = {"name": "GUI_Window", "type": "QQuickApplicationWindow", "visible": True}
+gUI_Window_Rectangle = {"container": gUI_Window_QQuickApplicationWindow, "type": "Rectangle", "unnamed": 1, "visible": True}
